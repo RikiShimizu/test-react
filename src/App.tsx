af893d19ab4525b589293ba78ba744ce9b88a472
riki shimizu <fabulous.riki@gmail.com>
2020-06-10 17:09:01 +0900

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Test−React riki</a>
-//       </header>
-//     </div>
-//   );
-// }
+// import logo from './logo.svg';
 
 // 型定義
 type Item = {
@@ -22,13 +10,8 @@ type Item = {
 
 // 配列定義
 const items: Item[] = [
-    {
-        id: 1,
-        title: '一番高い商品'
-    }, {
-        id: 2,
-        title: '一番ださい商品'
-    }
+    {id: 1, title: '一番高い商品'}, 
+    {id: 2, title: '一番ださい商品'}
 ]
 
 const App: React.FC = () => {
