8dabd12474654b741ab31ea5bda94dfe340e57a1
riki shimizu <fabulous.riki@gmail.com>
2020-06-10 17:58:07 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
+import Child from './components/Child';
 import './App.scss';
-// import logo from './logo.svg';
 
-// 型定義
-type Item = {
-    id: number
-    title: string
-}
-
-// 配列定義
-const items: Item[] = [
-    {id: 1, title: '一番高い商品'}, 
-    {id: 2, title: '一番ださい商品'}
-]
-
-const App: React.FC = () => {
+const App: React.FC = () => { 
     return (
         <div className="App">
-            <ul>
-            {items.map((item: Item) => (
-                <li key={item.id}>{item.title}</li>
-            ))}
-            </ul>
+            <Child message="プロップスで渡した文字列">
+                <h1>適当な文字列</h1>
+            </Child>
         </div>
     )
 }
